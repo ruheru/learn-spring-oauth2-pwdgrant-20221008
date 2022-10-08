b30cc1a7a28800df7b7620b8a8db76565f9688bb
idkhairul.umam3 <khairul.umam3@jd.id>
2022-10-08 17:56:08 +0700

    first commit

---
+++ src/main/java/com/learn/oauth2/pwdgrant/LearnOauth2PwdgrantApplication.java
@@ -0,0 +1,13 @@
+package com.learn.oauth2.pwdgrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOauth2PwdgrantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOauth2PwdgrantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/oauth2/pwdgrant/LearnOauth2PwdgrantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.oauth2.pwdgrant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOauth2PwdgrantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
