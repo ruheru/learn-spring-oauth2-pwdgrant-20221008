4629f6f229d128c301e68945fe93bda6c42f1abe
idkhairul.umam3 <khairul.umam3@jd.id>
2022-10-08 18:58:34 +0700

    add authorization server

---
+++ learn-oauth2-pwdgrant-authorization-server/src/main/java/com/learn/oauth2/authorizationserver/learnoauth2pwdgrantauthorizationserver/LearnOauth2PwdgrantAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.learn.oauth2.authorizationserver.learnoauth2pwdgrantauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOauth2PwdgrantAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOauth2PwdgrantAuthorizationServerApplication.class, args);
+	}
+
+}
